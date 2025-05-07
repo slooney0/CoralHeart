5a52cdec16562504dc51b703baa25f1aed089b2d
slooney0 <slooney098@gmail.com>
2025-05-06 21:43:46 -0400

    [Implementation]
    Implemented player pointing towards the mouse.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,5 +1,6 @@
 using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
@@ -21,7 +22,11 @@ public class Player : MonoBehaviour
     /** Float that represents the players move speed */
     private float playerSpeed = 7f;
 
+    /** Float that represents the normalized move speed when moving at an angle */
     private float normalSpeed;
+
+    /** Vector representing the location of the mouse */
+    private Vector3 mouseInput;
 
     //Game object fields
 
@@ -38,6 +43,7 @@ public class Player : MonoBehaviour
     {
         dirX = Input.GetAxisRaw("Horizontal");
         dirY = Input.GetAxisRaw("Vertical");
+        mouseInput = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
         if (math.abs(dirY) > 0 && math.abs(dirX) > 0)
         {
@@ -48,5 +54,17 @@ public class Player : MonoBehaviour
         {
             rb.linearVelocity = new Vector3(dirX * playerSpeed, dirY * playerSpeed, 0);
         }
+
+        Vector2 direction = (mouseInput - transform.position).normalized;
+
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, 0, angle), .25f);
+
+    }
+
+    public void OnMouseMove(InputAction.CallbackContext context)
+    {
+        mouseInput = context.ReadValue<Vector2>();
     }
 }
